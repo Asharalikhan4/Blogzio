9b428f89deaeb14f6b477150f5347b0f9f7aa8f4
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2023-09-07 17:34:36 +0530

    CTA Updated

--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -2,12 +2,14 @@ import Logo from "../assets/Logo.svg";
 import HeroImg from "../assets/HeroImage.svg";
 import Post1Image from "../assets/posts/post1.jpg";
 import postProfileImage from "../assets/posts/post-profile.svg";
+import CtaImage from "../assets/CtaImage.jpg";
 
 const images = {
     Logo,
     HeroImg,
     Post1Image,
     postProfileImage,
+    CtaImage,
 };
 
 export default images;

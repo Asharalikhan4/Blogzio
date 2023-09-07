a3fee89363bd2e82d48688701aaf32d87ffc916c
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2023-09-08 02:34:39 +0530

    Comment section completed

--- frontend/src/pages/articleDetail/ArticleDetailPage.jsx
+++ frontend/src/pages/articleDetail/ArticleDetailPage.jsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
-import BreadCrumbs from "../../components/BreadCrumbs";
 import MainLayout from "../../components/MainLayout";
+import BreadCrumbs from "../../components/BreadCrumbs";
 import { images } from "../../constants";
 import SuggestedPosts from "./container/SuggestedPosts";
 import CommentsContainer from "../../components/comments/CommentsContainer";
+import SocialShareButton from "../../components/SocialShareButton";
 
-const BreadCrumbsData = [
-  {name: "Home", link: "/"},
-  {name: "Blog", link: "/blog"},
-  {name: "Article title", link: "/blog/1"}
+const breadCrumbsData = [
+  { name: "Home", Link: "/" },
+  { name: "Blog", Link: "/blog" },
+  { name: "Article title", Link: "/blog/1" },
 ];
 
 const postData = [
@@ -51,25 +52,51 @@ const tagsData = [
 const ArticleDetailPage = () => {
   return (
     <MainLayout>
-        <section className="container mx-auto max-w-5xl flex flex-col px-5 py-5 lg:flex-row lg:gap-x-5 lg:items-start">
-            <article className="flex-1">
-              <BreadCrumbs data={BreadCrumbsData}/>
-              <img className="rounded-xl w-full" src={images.Post1Image} alt="" />
-              <Link to="/blog?category=selectedCategory" className="text-primary text-sm font-roboto inline-block mt-4 md:text-base">EDUCATION</Link>
-              <h1 className="text-xl font-medium font-roboto mt-4 text-dark-hard md:text-[26px]">
-                Help children get better education
-              </h1>
-              <div className="mt-4 text-dark-soft">
-                <p className="leading-7">
-                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum a obcaecati aliquam iure debitis est voluptates perspiciatis, laborum neque, aperiam libero eveniet saepe reiciendis cum. Error maiores consectetur totam ex aliquid expedita minus, quia maxime eos repellendus libero nobis architecto optio, animi deleniti rerum quam labore magni soluta distinctio? Nobis!
-                </p>
-              </div>
-              <CommentsContainer className="mt-10" />
-            </article>
-            <SuggestedPosts header="Latest Article" posts={postData} tags={tagsData} className="mt-8 lg:mt-0 lg:max-w-xs"/>
-        </section>
+      <section className="container mx-auto max-w-5xl flex flex-col px-5 py-5 lg:flex-row lg:gap-x-5 lg:items-start ">
+        <article className="flex-1">
+          <BreadCrumbs data={breadCrumbsData} />
+          <img
+            className="rounded-xl w-full"
+            src={images.Post1Image}
+            alt="laptop"
+          />
+          <Link
+            to="/blog?category=selectedCategory"
+            className="text-primary text-sm font-roboto inline-block mt-4 md:text-base"
+          >
+            Education
+          </Link>
+          <h1 className="text-xl font-medium font-roboto mt-4 text-dark-hard md:text-[26px]">
+            Help children get better education
+          </h1>
+          <div className="mt-4 text-dark-soft">
+            <p className="leading-7">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga
+              voluptas numquam repudiandae voluptatum, adipisci aliquam cum eos
+              veniam minima, laboriosam eius expedita ad debitis suscipit
+              nesciunt labore aut cupiditate saepe corporis, quidem illum alias
+              nam molestias quae. Quae, debitis. Minus!
+            </p>
+          </div>
+          <CommentsContainer className="mt-10" logginedUserId="a"/>
+        </article>
+        <div>
+        <SuggestedPosts
+          header="Latest Article"
+          posts={postData}
+          tags={tagsData}
+          className="mt-8 lg:mt-0 lg:max-w-xs"
+        />
+        <div className="mt-7">
+          <h2 className="font-roboto font-medium text-dark-hard mb-4 md:text-xl">
+            Share on:
+          </h2>
+          <SocialShareButton url={encodeURI("https://wwww.google.com")} title={encodeURIComponent("Google Search Engine")}/>
+        </div>
+        </div>
+      </section>
     </MainLayout>
-  )
-}
+  );
+};
 
 export default ArticleDetailPage;

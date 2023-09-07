1a1d8b8c0c8dd7accf4e690e02e0c4f55ab432d1
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2023-09-08 04:49:30 +0530

    Update profile added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 const router = express.Router();
 
-import { loginUser, registerUser, userProfile } from "../controllers/userController";
+import { loginUser, registerUser, updateProfile, userProfile } from "../controllers/userController";
 import { authGuard } from "../middlewares/authMiddleware";
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/profile", authGuard, userProfile);
+router.put("/updateProfile", authGuard, updateProfile);
 
 export default router;

2619ab9cdd9be5bdd2acbd2ae6b5b68fa2e48fa8
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2023-09-07 17:26:51 +0530

    CTA Section added

--- frontend/src/pages/Home/HomePage.jsx
+++ frontend/src/pages/Home/HomePage.jsx
@@ -1,9 +1,11 @@
 import MainLayout from "../../components/MainLayout";
+import Articles from "./container/Articles";
 import Hero from "./container/Hero";
 
 const HomePage = () => {
   return <MainLayout>
     <Hero />
+    <Articles />
   </MainLayout>
 }
 

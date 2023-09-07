fa2942dc6333dd7ca972daec0b9ca97cb2e2582f
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2023-09-08 01:08:09 +0530

    Article detail page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+
 import "./App.css";
 import HomePage from "./pages/Home/HomePage";
+import ArticleDetailPage from "./pages/articleDetail/ArticleDetailPage";
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
+    <div className="App font-opensans">
+      <Routes>
+        <Route index path="/" element={<HomePage />} />
+        <Route index path="/blog/:id" element={<ArticleDetailPage />} />
+      </Routes>
     </div>
   )
 }

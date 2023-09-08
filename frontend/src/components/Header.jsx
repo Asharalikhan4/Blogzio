a948564ce4b7ca3a9ec5900cf353dd88314905f8
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2023-09-08 15:06:51 +0530

    Logout api integrated

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import { MdKeyboardArrowDown } from "react-icons/md";
 
-
 import { images } from "../constants";
+import { logout } from "../store/actions/user";
 
 const navItemsInfo = [
     { name: "Home", type: "link" },
@@ -30,7 +31,7 @@ const NavItem = ({ item }) => {
                 <span className="cursor-pointer text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">/</span>
             </>) : (
                 <div className="flex flex-col items-center">
-                    <button href="/" className="px-4 py-2 flex gap-x-1 items-center" onClick={toggleDropdownHandler}>{item.name}<MdKeyboardArrowDown /></button>
+                    <button href="/" className="px-4 py-2 flex gap-x-1 items-center" onClick={toggleDropdownHandler}><span>{item.name}</span><MdKeyboardArrowDown /></button>
                     <div className={`${dropdown ? "block" : "hidden"} lg:hidden transition-all duration-500 pt-4 lg:absolute lg:bottom-0 lg:right-0 lg:transform lg:translate-y-full lg:group-hover:block w-max`}>
                         <ul className="bg-dark-soft lg:bg-transparent text-center flex flex-col shadow-lg rounded-lg overflow-hidden">
                             {item.items.map((page, index) => (
@@ -48,13 +49,22 @@ const NavItem = ({ item }) => {
 
 const Header = () => {
 
+    const dispatch = useDispatch();
+
+    const userState = useSelector(state => state.user);
+
     const [navIsVisible, setNavIsVisible] = useState(false);
+    const [profileDropdown, setProfileDropdown] = useState(false);
 
     const navVisibilityHandler = () => {
         setNavIsVisible((curState) => {
             return !curState;
         });
     };
+
+    const logoutHandler = () => {
+        dispatch(logout());
+    }
 
     return (
         <section className="sticky top-0 left-0 right-0 z-50 bg-white">
@@ -71,7 +81,27 @@ const Header = () => {
                             <NavItem key={item.name} item={item} />
                         ))}
                     </ul>
-                    <button className="mt-5 lg:mt-0 border-2 border-blue-500 px-6 py-3 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300">Sign in</button>
+                    {userState.userInfo ? (
+                        <div className="text-white items-center gap-y-5 lg:text-dark-soft flex flex-col lg:flex-row gap-x-2 font-semibold">
+                            <div className="relative group">
+                            <div className="flex flex-col items-center">
+                                <button href="/" className="flex gap-x-1 items-center mt-5 lg:mt-0 border-2 border-blue-500 px-6 py-3 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300" onClick={() => setProfileDropdown(!profileDropdown)}><span>Profile</span><MdKeyboardArrowDown /></button>
+                                <div className={`${profileDropdown ? "block" : "hidden"} lg:hidden transition-all duration-500 pt-4 lg:absolute lg:bottom-0 lg:right-0 lg:transform lg:translate-y-full lg:group-hover:block w-max`}>
+                                    <ul className="bg-dark-soft lg:bg-transparent text-center flex flex-col shadow-lg rounded-lg overflow-hidden">
+                                        <button type="button" className="hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft">
+                                            Dashboard
+                                        </button>
+                                        <button onClick={logoutHandler} type="button" className="hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft">
+                                            Logout
+                                        </button>
+                                    </ul>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                    ) : (
+                        <button className="mt-5 lg:mt-0 border-2 border-blue-500 px-6 py-3 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300">Sign in</button>
+                    )}
                 </div>
             </header>
         </section>

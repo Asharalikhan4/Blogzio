6ee279ab5b897fe8d549aad4092640d6c2a52d7a
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2023-09-09 02:14:31 +0530

    Blog api connect

--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -3,6 +3,8 @@ import HeroImg from "../assets/HeroImage.svg";
 import Post1Image from "../assets/posts/post1.jpg";
 import postProfileImage from "../assets/posts/post-profile.svg";
 import CtaImage from "../assets/CtaImage.jpg";
+import samplePostImage from "../../public/images/sample.jpg";
+import userImage from "../../public/images/user.png";
 
 const images = {
     Logo,
@@ -10,6 +12,8 @@ const images = {
     Post1Image,
     postProfileImage,
     CtaImage,
+    samplePostImage,
+    userImage,
 };
 
 export default images;

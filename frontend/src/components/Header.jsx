cff28a30f1d7f2fde4cb9fa56181a6ab0d564444
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2023-09-08 19:29:09 +0530

    Profile photo delete implemented

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,11 +8,11 @@ import { images } from "../constants";
 import { logout } from "../store/actions/user";
 
 const navItemsInfo = [
-    { name: "Home", type: "link" },
-    { name: "Articles", type: "link" },
-    { name: "Pages", type: "dropdown", items: ["About us", "Contact us"] },
-    { name: "Pricing", type: "link" },
-    { name: "Faq", type: "link" },
+    { name: "Home", type: "link", href: "/" },
+    { name: "Articles", type: "link", href: "/articles" },
+    { name: "Pages", type: "dropdown", items: [{title: "About us", href: "/about"}, {title: "Contact us", href: "/contact"}] },
+    { name: "Pricing", type: "link", href: "/pricing" },
+    { name: "Faq", type: "link", href: "/faq" },
 ];
 
 const NavItem = ({ item }) => {
@@ -28,7 +28,7 @@ const NavItem = ({ item }) => {
     return (
         <li className="relative group">
             {item.type === "link" ? (<>
-                <a href="/" className="px-4 py-2">{item.name}</a>
+                <Link to={item.href} className="px-4 py-2">{item.name}</Link>
                 <span className="cursor-pointer text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">/</span>
             </>) : (
                 <div className="flex flex-col items-center">
@@ -36,9 +36,9 @@ const NavItem = ({ item }) => {
                     <div className={`${dropdown ? "block" : "hidden"} lg:hidden transition-all duration-500 pt-4 lg:absolute lg:bottom-0 lg:right-0 lg:transform lg:translate-y-full lg:group-hover:block w-max`}>
                         <ul className="bg-dark-soft lg:bg-transparent text-center flex flex-col shadow-lg rounded-lg overflow-hidden">
                             {item.items.map((page, index) => (
-                                <a key={index} href="/" className="hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft">
-                                    {page}
-                                </a>
+                                <Link to={page.href} key={index} className="hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft">
+                                    {page.title}
+                                </Link>
                             ))}
                         </ul>
                     </div>
@@ -71,9 +71,9 @@ const Header = () => {
     return (
         <section className="sticky top-0 left-0 right-0 z-50 bg-white">
             <header className="container mx-auto px-5 flex justify-between py-4 items-center">
-                <div>
-                    <Link to="/"><img className="w-16" src={images.Logo} alt="logo" /></Link>
-                </div>
+                <Link to="/">
+                    <img className="w-16" src={images.Logo} alt="logo" />
+                </Link>
                 <div className="lg:hidden z-50">
                     {navIsVisible ? <AiOutlineClose className="w-6 h-6" onClick={navVisibilityHandler} /> : <AiOutlineMenu className="w-6 h-6" onClick={navVisibilityHandler} />}
                 </div>
